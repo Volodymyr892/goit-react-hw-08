60be0bf234fa8f6c01d96b1aad1e9e42bb64665c
Volodymyr892 <sevcukvolodimir896@gmail.com>
2024-08-04 17:00:26 +0300

    d

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,17 +3,14 @@ import { deleteContact } from "../../redux/contacts/operations"
 
 export default function Contact({ contact:{name,number,id} }) {
     const dispatch = useDispatch()
-    const handleDelete = (id) => {
-        console.log('Deleting contact with ID:', id); // Додати цю лінію для перевірки
-        dispatch(deleteContact(id));
-      };
+    const handleDelete = () => dispatch(deleteContact(id));
     return(
             <>
                 <div>
                     <p>Name {name}</p>
                     <p>Number {number}</p>
                 </div>
-                <button onClick={() =>handleDelete(id)}>Delete</button>
+                <button type="button" onClick={handleDelete}>Delete</button>
             </>
     )
 }
